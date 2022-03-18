6f06718d55142e37d223563ea716b09af56684d3
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2022-03-18 20:47:39 +0300

    splice in ex 4 task 11 has been fixed

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -2,19 +2,18 @@ function calcIdenticalSymbols(str) {
   const strArr = str.split('');
 
   for (let i = 0; i < strArr.length; i += 1) {
-    let identicalSymbols = 0;
+    let identicalSymbols = 1;
 
     for (let j = 0; j < strArr.length; j += 1) {
-      if (strArr[i] === strArr[j]) {
+      if (strArr[i] === strArr[j] && i !== j) {
         identicalSymbols += 1;
-
-        if (i !== j) {
-          strArr.splice(j, 1);
-        }
+        delete strArr[j];
       }
     }
 
-    console.log(`Cимволов ${strArr[i]} в строке: ${identicalSymbols}`);
+    if (strArr[i] !== undefined) {
+      console.log(`Cимволов ${strArr[i]} в строке: ${identicalSymbols}`);
+    }
   }
 }
 
