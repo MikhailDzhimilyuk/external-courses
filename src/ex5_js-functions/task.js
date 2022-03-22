0a033dcd465f46755fcdf75e52eee7fb1ffbf82c
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2022-03-22 15:45:43 +0300

    ex5 task is done

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,50 @@
+function calc() {
+  let res = 0;
+
+  return {
+    add: function add(x) {
+      if (!isNaN(x)) {
+        res += x;
+      }
+
+      return add;
+    },
+    subtract: function subtract(x) {
+      if (!isNaN(x)) {
+        res -= x;
+      }
+
+      return subtract;
+    },
+    divide: function divide(x) {
+      if (!isNaN(x)) {
+        res /= x;
+      }
+
+      return divide;
+    },
+    multiply: function multiply(x) {
+      if (!isNaN(x)) {
+        res *= x;
+      }
+
+      return multiply;
+    },
+    getResult() {
+      if (isFinite(res)) { return res; }
+
+      res = 'Ошибка';
+
+      return res;
+    },
+    reset() {
+      res = 0;
+
+      return res;
+    },
+  };
+}
+
+const calculator = calc();
+
+module.exports = calculator;
