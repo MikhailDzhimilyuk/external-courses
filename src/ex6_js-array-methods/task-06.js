1a9e58c242c280ff16df0216751609ff62ce2b4b
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2022-03-26 17:53:43 +0300

    ex6 task is redone

--- src/ex6_js-array-methods/task-06.js
+++ src/ex6_js-array-methods/task-06.js
@@ -1,15 +1,14 @@
 function reduce(array, callback, initialValue) {
   let previousValue = array[0];
+  let i = 1;
 
-  for (let i = 1; i < array.length; i += 1) {
-    if (initialValue && previousValue === array[0]) {
-      previousValue = initialValue;
-      i -= 1;
-    }
+  if (initialValue) {
+    previousValue = initialValue;
+    i -= 1;
+  }
 
-    const currentItem = array[i];
-
-    previousValue = callback(previousValue, currentItem, i, array);
+  for (; i < array.length; i += 1) {
+    previousValue = callback(previousValue, array[i], i, array);
   }
 
   return previousValue;
