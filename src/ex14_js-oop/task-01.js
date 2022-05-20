2b394a7512a2ce2a19b0503edd8fe34d57cf5f10
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2022-05-21 00:47:14 +0300

    Исправил названия массивов

--- src/ex14_js-oop/task-01.js
+++ src/ex14_js-oop/task-01.js
@@ -5,25 +5,25 @@ function electricalDevice (power, switchOnOff) {
     this.changeSwitch = function(switchOnOff) {
         this.switchOnOff = switchOnOff;
         if (this.switchOnOff == "off") { this.power = 0; } else { this.power = power; }
-        this.info[0] = `Power: ${this.power} W`;
-        this.info[1] = `Switch: ${this.switchOnOff}`;
+        this.infoArr[0] = `Power: ${this.power} W`;
+        this.infoArr[1] = `Switch: ${this.switchOnOff}`;
         console.log(`Switch ${this.switchOnOff}`);
     };
 
-    this.info = [`Power: ${this.power} W`, `Switch: ${this.switchOnOff}`,];
+    this.infoArr = [`Power: ${this.power} W`, `Switch: ${this.switchOnOff}`,];
 
-    this.getInfo = function() { console.log(this); console.log(this.info); };
+    this.getInfo = function() { console.log(this); console.log(this.infoArr); };
 }
 
 function vacuumCleaner (power, switchOnOff, cleaningMode) {
     electricalDevice.call(this, power, switchOnOff);
     this.cleaningMode = cleaningMode;
-    this.info.push(`Cleaning mode: ${cleaningMode}`);
+    this.infoArr.push(`Cleaning mode: ${cleaningMode}`);
 
     this.changeCleaningMode = function(cleaningMode) {
         if (this.switchOnOff == 'on') {
             this.cleaningMode = cleaningMode;
-            this.info[this.info.length - 1] = `Cleaning mode: ${cleaningMode}`;
+            this.infoArr[this.infoArr.length - 1] = `Cleaning mode: ${cleaningMode}`;
             console.log(`Cleaning Mode: ${this.cleaningMode}`);
         }
     }
@@ -35,20 +35,20 @@ vacuumCleaner.prototype.constructor = vacuumCleaner;
 function robotCleaner (power, switchOnOff, cleaningMode, scanningTerrain) {
     vacuumCleaner.call(this, power, switchOnOff, cleaningMode);
     this.scanningTerrain = scanningTerrain;
-    this.info.push('Scanning terrain: map is not defined');
+    this.infoArr.push('Scanning terrain: map is not defined');
 
     if (this.scanningTerrain == 'on') {
-        setTimeout(() => { this.info[this.info.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
+        setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
     }
 
     this.changeScanningTerrain = function(scanningTerrain) {
         if (this.switchOnOff == 'on') {
             this.scanningTerrain = scanningTerrain;
-            this.info[this.info.length - 1] = 'Scanning terrain: map is not defined';
+            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
             console.log(`Scanning Terrain: ${this.scanningTerrain}`);
 
             if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.info[this.info.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
+                setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
             } 
         }
     }
@@ -60,22 +60,22 @@ robotCleaner.prototype.constructor = robotCleaner;
 function robotSoldier (power, switchOnOff, scanningTerrain, fire) {
     electricalDevice.call(this, power, switchOnOff);
     this.scanningTerrain = scanningTerrain;
-    this.info.push('Scanning terrain: map is not defined');
+    this.infoArr.push('Scanning terrain: map is not defined');
 
     if (this.scanningTerrain == 'on') {
-        setTimeout(() => { this.info[this.info.length - 2] = 'Scanning terrain: map is defined'; }, 1000) 
+        setTimeout(() => { this.infoArr[this.infoArr.length - 2] = 'Scanning terrain: map is defined'; }, 1000) 
     }
 
     this.fire = fire;
-    this.info.push(`fire: ${fire}`);
+    this.infoArr.push(`fire: ${fire}`);
 
     this.changeScanningTerrain = function(scanningTerrain) {
         if (this.switchOnOff == 'on') {
             this.scanningTerrain = scanningTerrain;
-            this.info[this.info.length - 1] = 'Scanning terrain: map is not defined';
+            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
             console.log(`Scanning Terrain: ${this.scanningTerrain}`);
             if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.info[this.info.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
+                setTimeout(() => { this.infoArr[this.infoArr.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
             } 
         }
     }
@@ -83,7 +83,7 @@ function robotSoldier (power, switchOnOff, scanningTerrain, fire) {
     this.changeFire = function(fire) {
         if (this.switchOnOff == 'on') {
             this.fire = fire;
-            this.info[this.info.length - 1] = `fire: ${fire}`;
+            this.infoArr[this.infoArr.length - 1] = `fire: ${fire}`;
             console.log(`Fire: ${fire}`);
         }
     }
--- src/ex14_js-oop/task-02.js
+++ src/ex14_js-oop/task-02.js
@@ -2,7 +2,7 @@ class ElectricalDevice {
     name;
     power;
     switchOnOff;
-    info;    
+    infoArr;   
 
     constructor(name, power, switchOnOff) {
         this.name = name;
@@ -11,18 +11,18 @@ class ElectricalDevice {
 
         if (this.switchOnOff == 'off') {  this.powerOn = this.power; this.power = 0; }
 
-        this.info = [`Power: ${this.power} W`, `Switch: ${this.switchOnOff}`,];
+        this.infoArr = [`Power: ${this.power} W`, `Switch: ${this.switchOnOff}`,];
     }
 
     changeSwitch(switchOnOff) {
         this.switchOnOff = switchOnOff;
         if (this.switchOnOff == "off") { this.power = 0; } else { this.power = this.powerOn; }
-        this.info[0] = `Power: ${this.power} W`;
-        this.info[1] = `Switch: ${this.switchOnOff}`;
+        this.infoArr[0] = `Power: ${this.power} W`;
+        this.infoArr[1] = `Switch: ${this.switchOnOff}`;
         console.log(`Switch ${this.switchOnOff}`);
     }
 
-    getInfo() { console.log(this); console.log(this.info); };
+    getInfo() { console.log(this); console.log(this.infoArr); };
 
     searchElecDevice() {
         console.log(`Привет, меня зовут ${this.name} и я в комнате.`);
@@ -35,13 +35,13 @@ class VacuumCleaner extends ElectricalDevice {
     constructor(name, power, switchOnOff, cleaningMode) {
         super(name, power, switchOnOff);
         this.cleaningMode = cleaningMode;
-        this.info.push(`Cleaning mode: ${cleaningMode}`);
+        this.infoArr.push(`Cleaning mode: ${cleaningMode}`);
     }
 
     changeCleaningMode(cleaningMode) {
         if (this.switchOnOff == 'on') {
             this.cleaningMode = cleaningMode;
-            this.info[this.info.length - 1] = `Cleaning mode: ${cleaningMode}`;
+            this.infoArr[this.infoArr.length - 1] = `Cleaning mode: ${cleaningMode}`;
             console.log(`Cleaning Mode: ${this.cleaningMode}`);
         }
     }
@@ -53,21 +53,21 @@ class RobotCleaner extends VacuumCleaner {
     constructor(name, power, switchOnOff, cleaningMode, scanningTerrain) {
         super(name, power, switchOnOff, cleaningMode);
         this.scanningTerrain = scanningTerrain;
-        this.info.push('Scanning terrain: map is not defined');
+        this.infoArr.push('Scanning terrain: map is not defined');
 
         if (this.scanningTerrain == 'on') {
-            setTimeout(() => { this.info[this.info.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
+            setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
         }
     }
 
     changeScanningTerrain(scanningTerrain) {
         if (this.switchOnOff == 'on') {
             this.scanningTerrain = scanningTerrain;
-            this.info[this.info.length - 1] = 'Scanning terrain: map is not defined';
+            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
             console.log(`Scanning Terrain: ${this.scanningTerrain}`);
 
             if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.info[this.info.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
+                setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
             } 
         }
     }
@@ -84,23 +84,23 @@ class RobotSoldier extends ElectricalDevice {
     constructor(name, power, switchOnOff, scanningTerrain, fire) {
         super(name, power, switchOnOff);
         this.scanningTerrain = scanningTerrain;
-        this.info.push('Scanning terrain: map is not defined');
+        this.infoArr.push('Scanning terrain: map is not defined');
 
         if (this.scanningTerrain == 'on') {
-            setTimeout(() => { this.info[this.info.length - 1] = 'Scanning terrain: map is defined'; }, 1000) 
+            setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 1000) 
         }
 
         this.fire = fire;
-        this.info.push(`fire: ${fire}`);
+        this.infoArr.push(`fire: ${fire}`);
     }
 
     changeScanningTerrain(scanningTerrain) {
         if (this.switchOnOff == 'on') {
             this.scanningTerrain = scanningTerrain;
-            this.info[this.info.length - 1] = 'Scanning terrain: map is not defined';
+            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
             console.log(`Scanning Terrain: ${this.scanningTerrain}`);
             if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.info[this.info.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
+                setTimeout(() => { this.infoArr[this.infoArr.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
             } 
         }
     }
@@ -108,7 +108,7 @@ class RobotSoldier extends ElectricalDevice {
     changeFire(fire) {
         if (this.switchOnOff == 'on') {
             this.fire = fire;
-            this.info[this.info.length - 1] = `fire: ${fire}`;
+            this.infoArr[this.infoArr.length - 1] = `fire: ${fire}`;
             console.log(`Fire: ${fire}`);
         }
     }
@@ -118,9 +118,8 @@ let VacuumCleaner1 = new VacuumCleaner('Пылесос', 1000, 'on', 'сухая
 let RobotCleaner1 = new RobotCleaner('Робот-пылесос', 2000, 'off', 'сухая', 'on');
 let RobotSoldier1 = new RobotSoldier('Робот-солдат Арни', 10000, 'on', 'on', 'on');
 
-const electricalDevices = [VacuumCleaner1.power, RobotCleaner1.power, RobotSoldier1.power];
-let summaryPower = electricalDevices.map(i => x+=i, x=0).reverse()[0];
-
+const electricalDevicesArr = [VacuumCleaner1.power, RobotCleaner1.power, RobotSoldier1.power];
+let summaryPower = electricalDevicesArr.map(i => x+=i, x=0).reverse()[0];
 console.log(summaryPower);
 VacuumCleaner1.searchElecDevice();
 RobotCleaner1.searchElecDevice();
