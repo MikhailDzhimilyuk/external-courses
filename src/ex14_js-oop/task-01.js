eeea753f621c7ccffbc0c94f62361b903dca1d1f
MikhailDzhimilyuk <mdzhimilyuk@mail.com>
2022-05-22 23:06:42 +0300

    Сделал прототипное наследование по образцу из mdn

--- src/ex14_js-oop/task-01.js
+++ src/ex14_js-oop/task-01.js
@@ -1,32 +1,27 @@
 function ElectricalDevice (power, switchOnOff) {
     this.switchOnOff = switchOnOff;
-    this.power = power;
-
-    this.changeSwitch = function(switchOnOff) {
-        this.switchOnOff = switchOnOff;
-        if (this.switchOnOff == "off") { this.power = 0; } else { this.power = power; }
-        this.infoArr[0] = `Power: ${this.power} W`;
-        this.infoArr[1] = `Switch: ${this.switchOnOff}`;
-        console.log(`Switch ${this.switchOnOff}`);
-    };
-
+    this.power = this.powerOn = power;
     this.infoArr = [`Power: ${this.power} W`, `Switch: ${this.switchOnOff}`,];
+}
 
-    this.getInfo = function() { console.log(this); console.log(this.infoArr); };
+ElectricalDevice.prototype.changeSwitch = function(switchOnOff) {
+    this.switchOnOff = switchOnOff;
+
+    if (this.switchOnOff == "off") { this.power = 0; } else { this.power = this.powerOn; }
+
+    this.infoArr[0] = `Power: ${this.power} W`;
+    this.infoArr[1] = `Switch: ${this.switchOnOff}`;
+    console.log(`Switch ${this.switchOnOff}`);
+}
+
+ElectricalDevice.prototype.getInfo = function() {
+    console.log(this); console.log(this.infoArr);
 }
 
 function VacuumCleaner (power, switchOnOff, cleaningMode) {
     ElectricalDevice.call(this, power, switchOnOff);
     this.cleaningMode = cleaningMode;
     this.infoArr.push(`Cleaning mode: ${cleaningMode}`);
-
-    this.changeCleaningMode = function(cleaningMode) {
-        if (this.switchOnOff == 'on') {
-            this.cleaningMode = cleaningMode;
-            this.infoArr[this.infoArr.length - 1] = `Cleaning mode: ${cleaningMode}`;
-            console.log(`Cleaning Mode: ${this.cleaningMode}`);
-        }
-    }
 }
 
 VacuumCleaner.prototype = Object.create(ElectricalDevice.prototype);
@@ -36,6 +31,14 @@ Object.defineProperty(VacuumCleaner.prototype, 'constructor', {
     writable: true 
 });
 
+VacuumCleaner.prototype.changeCleaningMode = function(cleaningMode) {
+    if (this.switchOnOff == 'on') {
+        this.cleaningMode = cleaningMode;
+        this.infoArr[this.infoArr.length - 1] = `Cleaning mode: ${cleaningMode}`;
+        console.log(`Cleaning Mode: ${this.cleaningMode}`);
+    }
+}
+
 function RobotCleaner (power, switchOnOff, cleaningMode, scanningTerrain) {
     VacuumCleaner.call(this, power, switchOnOff, cleaningMode);
     this.scanningTerrain = scanningTerrain;
@@ -43,18 +46,6 @@ function RobotCleaner (power, switchOnOff, cleaningMode, scanningTerrain) {
 
     if (this.scanningTerrain == 'on') {
         setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
-    }
-
-    this.changeScanningTerrain = function(scanningTerrain) {
-        if (this.switchOnOff == 'on') {
-            this.scanningTerrain = scanningTerrain;
-            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
-            console.log(`Scanning Terrain: ${this.scanningTerrain}`);
-
-            if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
-            } 
-        }
     }
 }
 
@@ -64,6 +55,18 @@ Object.defineProperty(RobotCleaner.prototype, 'constructor', {
     enumerable: false,
     writable: true 
 });
+
+RobotCleaner.prototype.changeScanningTerrain = function(scanningTerrain) {
+    if (this.switchOnOff == 'on') {
+        this.scanningTerrain = scanningTerrain;
+        this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
+        console.log(`Scanning Terrain: ${this.scanningTerrain}`);
+
+        if (this.scanningTerrain == 'on') { 
+            setTimeout(() => { this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is defined'; }, 3000) 
+        } 
+    }
+}
 
 function RobotSoldier (power, switchOnOff, scanningTerrain, fire) {
     ElectricalDevice.call(this, power, switchOnOff);
@@ -76,25 +79,6 @@ function RobotSoldier (power, switchOnOff, scanningTerrain, fire) {
 
     this.fire = fire;
     this.infoArr.push(`fire: ${fire}`);
-
-    this.changeScanningTerrain = function(scanningTerrain) {
-        if (this.switchOnOff == 'on') {
-            this.scanningTerrain = scanningTerrain;
-            this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
-            console.log(`Scanning Terrain: ${this.scanningTerrain}`);
-            if (this.scanningTerrain == 'on') { 
-                setTimeout(() => { this.infoArr[this.infoArr.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
-            } 
-        }
-    }
-
-    this.changeFire = function(fire) {
-        if (this.switchOnOff == 'on') {
-            this.fire = fire;
-            this.infoArr[this.infoArr.length - 1] = `fire: ${fire}`;
-            console.log(`Fire: ${fire}`);
-        }
-    }
 }
 
 RobotSoldier.prototype = Object.create(ElectricalDevice.prototype);
@@ -103,3 +87,22 @@ Object.defineProperty(RobotSoldier.prototype, 'constructor', {
     enumerable: false,
     writable: true 
 });
+
+RobotSoldier.prototype.changeScanningTerrain = function(scanningTerrain) {
+    if (this.switchOnOff == 'on') {
+        this.scanningTerrain = scanningTerrain;
+        this.infoArr[this.infoArr.length - 1] = 'Scanning terrain: map is not defined';
+        console.log(`Scanning Terrain: ${this.scanningTerrain}`);
+        if (this.scanningTerrain == 'on') { 
+            setTimeout(() => { this.infoArr[this.infoArr.length - 2] = 'Scanning terrain: map is defined'; }, 3000) 
+        } 
+    }
+}
+
+RobotSoldier.prototype.changeFire = function(fire) {
+    if (this.switchOnOff == 'on') {
+        this.fire = fire;
+        this.infoArr[this.infoArr.length - 1] = `fire: ${fire}`;
+        console.log(`Fire: ${fire}`);
+    }
+}
